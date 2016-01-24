01b0a9c95c080bc2130313defd090ba3f72a21a8
George Holevas <George@GMBP.home>
2016-01-23 19:30:27 -0500

    made tweets and names load from database

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,28 @@
 var express = require('express');
 var router = express.Router();
 // could use one line instead: var router = require('express').Router();
-var tweetBank = require('../tweetBank');
+//var tweetBank = require('../tweetBank');
+var newTweetBank = require('../models/index.js');
 
 // module.exports = router;
 module.exports = function(io){
 	router.get('/', function (req, res) {
-	  var tweets = tweetBank.list();
-	  res.render( 'index', { title: "Jordan Meeker", tweets: tweets } );
+	  // var tweets = tweetBank.list();
+	  var tweetObject = newTweetBank.User.findAll({ include: [ newTweetBank.Tweet ] })
+		.then(function (user) {
+			// console.log(JSON.stringify(user))
+		    var tweets = [];
+		     for (var i = 0; i < user.length; i++) {
+			 	tweets.push(user[i].dataValues);
+			 };
+			console.log(tweets[0].Tweets[0].dataValues.tweet);
+			return tweets;
+		})
+		.then(function (tweets) {
+			// JSON.stringify(tweets);
+	 		res.render( 'index', { title: "George Holevas", tweets: tweets } );
+		});
+
 	});
 
 	router.get('/users/:name', function(req, res) {
